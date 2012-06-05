8dcbc35e61b042ffa4963dbddbdbce909d81a18a
L. Grondin <grondilu@yahoo.fr>
2012-06-05 23:53:02 +0200

    using recursive form for elliptic curve multiplication

--- EC.pm
+++ EC.pm
@@ -1,12 +1,11 @@
 #!/usr/bin/perl
 # elliptic curve cryptography in Perl
-
 package EC::Curves;
 use strict;
 use warnings;
 
 # secp256k1, http://www.oid-info.com/get/1.3.132.0.10
-use bigint;
+use bigint; # try => 'GMP';
 use constant secp256k1 => {
     p => hex('0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F'),
     b => hex('0x0000000000000000000000000000000000000000000000000000000000000007'),
@@ -17,7 +16,6 @@ use constant secp256k1 => {
 	hex('0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141'),
     ], 'Point'),
 };
-no bigint;
 
 package EC;
 use strict;
@@ -53,62 +51,54 @@ sub import {
     die "curve has nul discriminant" if Delta == 0;
 }
 
-{
-    use bigint;
-
-    sub Delta { -16*(4*$a**3 + 27*$b**2) }
-    sub check {
-	my $u = shift;
-	return $u unless @$u;
-	die "curve parameters are not defined" unless defined $a and defined $b and defined $p;
-	die "curve has nul discriminant" if Delta == 0;
-	die "point is not on elliptic curve" unless ($$u[1]**2 - $$u[0]**3 - $a*$$u[0] - $b) % $p == 0;
-	return bless $u, 'EC::Point';
-    }
-    sub Cmp {
-	my ($u, $v) = map { check $_ } @_;
-	return !@$v ? !@$u : !@$u ? !@$v : $$u[0] == $$v[0] && $$u[1] == $$v[1];
-    }
-    sub double {
-	my $u = check shift;
-	return $u unless @$u;
-	my $l = (3*$$u[0]**2 + $a) * inverse_mod(2 * $$u[1], $p) % $p;
-	my $x = $l**2 - 2*$$u[0];
-	return bless [ map { $_ % $p } $x, $l*($$u[0] - $x) - $$u[1] ], 'EC::Point';
-    }
-    sub add {
-	my ($u, $v) = eval { map check($_), @_ };
-	die "$@ in add" if $@;
-	return $u unless @$v;
-	return $v unless @$u;
-	return +($$u[1] + $$v[1]) % $p == 0 ? bless [], 'EC::Point' : double $u if $$u[0] % $p == $$v[0] % $p;
-	my $i = inverse_mod($$v[0] - $$u[0], $p);
-	my $l = ($$v[1] - $$u[1]) * $i % $p;
-	my $x = $l**2 - $$u[0] - $$v[0];
-	return bless [ map { $_ % $p } $x, $l*($$u[0] - $x) - $$u[1] ], 'EC::Point';
-    }
-    sub mult {
-	my $k = shift;
-	my $u = check shift;
-	$k %= $$u[2] if defined $$u[2];
-	return bless [], 'EC::Point' if $k == 0 or not @$u;
-	die "negative factor" if $k < 0;
-	my $k3 = 3*$k;
-	my $i = 1; $i *= 2 while $i <= $k3; $i /= 2;
-	my $x = $u;
-	while ( ($i/=2) > 1 ) {
-	    $x = double $x;
-	    $x = add $x, $u			if  ($k3 & $i) != 0 and ($k & $i) == 0;
-	    $x = add $x, [ $$u[0], -$$u[1] ]	if  ($k3 & $i) == 0 and ($k & $i) != 0;
-	}
-	return $x;
-    }
+sub Delta { -16*(4*$a**3 + 27*$b**2) }
+sub check {
+    my $u = shift;
+    return $u unless @$u;
+    die "curve parameters are not defined" unless defined $a and defined $b and defined $p;
+    die "curve has nul discriminant" if Delta == 0;
+    die "point is not on elliptic curve" unless ($$u[1]**2 - $$u[0]**3 - $a*$$u[0] - $b) % $p == 0;
+    return bless $u, 'EC::Point';
 }
+sub Cmp {
+    my ($u, $v) = map { check $_ } @_;
+    return !@$v ? !@$u : !@$u ? !@$v : $$u[0] == $$v[0] && $$u[1] == $$v[1];
+}
+sub double {
+    my $u = check shift;
+    return $u unless @$u;
+    my $l = (3*$$u[0]**2 + $a) * inverse_mod(2 * $$u[1], $p) % $p;
+    my $x = $l**2 - 2*$$u[0];
+    return bless [ map { $_ % $p } $x, $l*($$u[0] - $x) - $$u[1] ], 'EC::Point';
+}
+sub add {
+    my ($u, $v) = eval { map check($_), @_ };
+    die "$@ in add" if $@;
+    return $u unless @$v;
+    return $v unless @$u;
+    return +($$u[1] + $$v[1]) % $p == 0 ? bless [], 'EC::Point' : double $u if $$u[0] % $p == $$v[0] % $p;
+    my $i = inverse_mod($$v[0] - $$u[0], $p);
+    my $l = ($$v[1] - $$u[1]) * $i % $p;
+    my $x = $l**2 - $$u[0] - $$v[0];
+    return bless [ map { $_ % $p } $x, $l*($$u[0] - $x) - $$u[1] ], 'EC::Point';
+}
+sub mult {
+    my $k = shift;
+    my $u = shift;
+    return
+    $k == 1   ? $u :
+    $k == 2   ? double $u :
+    $k%2 == 0 ? double mult $k/2, $u :
+    add $u, double mult $k/2, $u
+    ;
+}
+
+no bigint;
 
 package EC::Point;
 use overload
 '+' => sub { $_[0]->add($_[1]) },
-'*' => sub { $_[2] ? $_->[1]->mult($_[0]) : $_[0]->mult($_[1]) },
+'*' => sub { EC::mult($_[2] ? @_[0,1] : @_[1,0]) },
 q("") => sub {
     my $_ = shift;
     return @$_ ?
@@ -118,7 +108,7 @@ q("") => sub {
 'bool' => sub { my $_ = shift; @$_ > 0 },
 ;
 
-package Math::BigInt;
+#package Math::BigInt;
 #no warnings 'redefine';
 #use overload
 #'*' => sub {
