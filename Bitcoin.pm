3187d2a3827654398308649277620d7fb4137662
L. Grondin <grondilu@yahoo.fr>
2012-06-12 01:35:11 +0200

    rethinking Class organisation

--- Bitcoin.pm
+++ Bitcoin.pm
@@ -1,68 +1,48 @@
 #!/usr/bin/perl -w
-package Bitcoin;
-@ISA = qw(Exporter);
-@EXPORT_OK = qw(BASE58 BTC);
 use v5.14;
+use strict;
+use warnings;
 
-use constant TEST	=> 0;
-use constant BASE58	=> qw{
-      1 2 3 4 5 6 7 8 9
-    A B C D E F G H   J K L M N   P Q R S T U V W X Y Z
-    a b c d e f g h i j k   m n o p q r s t u v w x y z
-};
+package Bitcoin;
+require EC::DSA;
+use Bitcoin::Base58;
 
-use constant {
-    BTC                 =>  "\x{0243}",  # Ƀ
-    GENESIS             => TEST ?
-    '00000007199508e34a9ff81e6ec0c477a4cccff2a4767a8eee39c11db367b008' :
-    '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f',
-    TIMES20090103       => 'The Times 03/Jan/2009 Chancellor on brink of second bailout for banks',
+our $data_dir	= $ENV{BITCOIN_DATA_DIR} // $ENV{HOME}.'/.bitcoin';
 
-    DATA_DIR		=> $ENV{HOME}.'/.bitcoin',
+package Bitcoin::Key;
+require Bitcoin::Util;
+our @ISA = qw( Bitcoin::Base58::Data    EC::DSA::PrivateKey );
+sub size() { 256 }
+sub version() { defined($ENV{BITCOIN_TEST}) ? 129 : 128 }
+sub value { bless shift->copy(), 'Math::BigInt'; }
 
-    PROOF_OF_WORK_LIMIT =>          32, # bits
-    MAX_BLOCK_SIZE	=>   1_000_000,
-    COIN		=> 100_000_000,
-    CENT		=>   1_000_000,
-    MAX_MONEY		=>       21e14,
-
-    DEFAULT_PORT	=>        8333,
-
-    DUMMY_PASSWD        => 'dummy password',
-};
-
-use constant CREDITS	=> 'Satoshi Nakamoto', 'Gavin Andersen', 'bitcoin developpers';
-use constant IRC	=> 'irc.lfnet.org', '6667', '#bitcoin'. (TEST ? 'TEST' : '');
-
-use constant CHECKPOINTS => {
-   '0000000069e244f73d78e8fd29ba2fd2ed618bd6fa2ee92559f542fdb26e7c1d' =>  11111,
-   '000000002dd5588a74784eaa7ab0507a18ad16a236e7b1ce69f00d7ddfb5d0a6' =>  33333,
-   '00000000001e1b4903550a0b96e9a9405c8a95f387162e4944e8d9fbe501cd6a' =>  68555,
-   '00000000006a49b14bcf27462068f1264c961f11fa2e0eddd2be0791e1d4124a' =>  70567,
-   '0000000000573993a3c9e41ce34471c079dcf5f52a0e824a81e7f953b8661a20' =>  74000,
-   '00000000000291ce28027faea320c8d2b054b2e0fe44a773f3eefb151d6bdc97' => 105000,
-   '000000000000774a7f8a7a12dc906ddb9e17e75d684f15e00f8767f9e8f36553' => 118000,
-   '00000000000005b12ffd4cd315cd34ffd4a594f430ac814c91184a0d42d2b0fe' => 134444,
-   '000000000000033b512028abb90e1626d8b346fd0ed598ac0a3c371138dce2bd' => 140700,
-};
-
-sub hash160 {
-    scalar qx/
-    perl -e 'print pack q(b*), "@{[unpack 'b*', shift]}"' |
-    openssl dgst -sha256 -binary |
-    openssl dgst -rmd160 -binary
-    /;
+sub new {
+    my $class = shift;
+    my $arg = shift;
+    if(not defined $arg) { return bless Bitcoin::Util::randInt(), $class }
+    elsif(ref $arg ~~ [qw(Math::BigInt EC::DSA::PrivateKey)]) { bless $arg, $class }
+    elsif($arg =~ m/\A$Bitcoin::Base58::b58+Z/i) {
+	my $new = bless $class->value_from_address($arg), $class;
+	die 'invalid key' unless $arg eq $new->to_base58;
+	return $new;
+    }
 }
-sub hash160_hex { unpack 'H*', hash160 @_ }
+sub address { Bitcoin::Address->new(shift->public_point) }
 
-sub hash;
-sub hash_hex;
-sub hash_int;
-{
-    use Digest::SHA qw(sha256);
-    sub hash     { sha256 sha256 shift }
-    sub hash_hex { unpack 'H*', reverse hash shift }
-    sub hash_int { use bigint; hex unpack 'H*', hash shift }
+package Bitcoin::Address;
+our @ISA = qw( Bitcoin::Base58::Data    EC::DSA::PublicKey );
+sub size() { 160 }
+sub version() { defined($ENV{BITCOIN_TEST}) ? 1 : 0 }
+sub data {
+    my $this = shift->_no_class;
+    use Bitcoin::Digest qw(hash160_bin);
+    hash160_bin pack 'H2H*',
+    '04', ((2**256+$this->x)*2**256+$this->y)->as_hex =~ s/0x1//r;
+}
+
+sub new {
+    my $class = shift;
+    my $arg = shift;
 }
 
 1;
@@ -75,10 +55,15 @@ Bitcoin
 
 =head1 SYNOPSIS
 
-    use Bitcoin qw(BASE58 BTC);
+    use Bitcoin;
 
-    printf "In a bitcoin address, authorized characters are: %s\n", join(', ', BASE58);
-    printf "You owe me %.2f%s.\n", 10, BTC;
+    say my $k = new Bitcoin::Key;
+    my $addr = new Bitcoin::Address "1456someSi11yBi1c6inAddressXnkjn56dxx"
+        or die "this is not a valid bitcoin address";
+    
+    say $k->address;
+    say $k->public_point;
+    my @signature = $k->sign("some message");
 
 =head1 DESCRIPTION
 
@@ -86,59 +71,9 @@ Bitcoin is a peer-to-peer electronic cash system created in 2009 by Satoshi Naka
 and its submodules implement several tools for bitcoin-related operations.  This is part of a project aiming
 at a full Perl implementation of the bitcoin protocol.
 
-This particular module implements bitcoin's specific constants and functions
-that didn't fit in any subcategories.
-
-=encoding utf8
-
-It currently contains:
-
-=over
-
-=item * the Ƀ character, defined as the C<BTC> constant ;
-
-=item * the base-58 charset (C<BASE58> list constant);
-
-=item * hash functions such as C<hash160> which is actually a SHA-256 followed by a RIPEMD-160 ;
-
-=item * The genesis hash code;
-
-=item * the blockchain checkpoints;
-
-=back
-
-The BASE58 constant has been put here instead of in Bitcoin::Base58 because
-Bitcoin::Base58::BASE58 would have been kind of redundant.
-
-Only BASE58 and BTC can be exported.
-
-=head1 SUBMODULES
-
-The Bitcoin module is not supposed to contain much.  Most useful stuffs are
-implemented in submodules.
-
-This section just sumarize their fonctionalities, and several other modules
-exist but are not documented here yet.  See their POD in the source code tree.
-
-=head2 Bitcoin::Database
-
-This class inherits from BerkeleyDB::Btree and provides default environnement
-for opening a bitcoin database used by the vanilla client.
-
-=head2 Bitcoin::Base58
-
-This module implements Satoshi's base58 encoding.  It also contains an abstract class
-implementing version and checksum coding used for Bitcoin addresses and private keys.
-See Bitcoin::Address and Bitcoin::Key::Secret.
-
-=head2 Bitcoin::Electrum
-
-Electrum is a bitcoin lightweight client/server architecture originally written
-in Python.  Hopefully someday this module will be a Perl implementation of Electrum.
-
 =head1 SEE ALSO
 
-Bitcoin::CNames, Bitcoin::Address, Bitcoin::Block, Bitcoin::Transaction, Bitcoin::Base58, EC
+Bitcoin::Key, Bitcoin::Address, Bitcoin::Block, Bitcoin::Transaction, Bitcoin::Base58, EC
 
 =head1 AUTHOR
 
--- Bitcoin/Base58.pm
+++ Bitcoin/Base58.pm
@@ -1,27 +1,32 @@
 #!/usr/bin/perl -w
 # Satoshi Nakamoto's encoding in Perl.
-package Bitcoin::Base58;
-require Exporter;
-@ISA = qw(Exporter);
-@EXPORT_OK = qw(encode decode);
-use Bitcoin;
-use Bitcoin::Util;
-
 use strict;
 use warnings;
+use feature qw(say);
+
+package Bitcoin::Base58;
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw(encode decode);
+use Bitcoin::Util;
 
 sub decode;
 sub encode;
 
-my %b58 = map { (Bitcoin::BASE58)[$_] => $_ } 0 .. 57;
-my $b58 = qr/[@{[Bitcoin::BASE58]}]/x;
+our @b58 = qw{
+      1 2 3 4 5 6 7 8 9
+    A B C D E F G H   J K L M N   P Q R S T U V W X Y Z
+    a b c d e f g h i j k   m n o p q r s t u v w x y z
+};
+our %b58 = map { $b58[$_] => $_ } 0 .. 57;
+our $b58 = qr/[@b58]/x;
 
 {
     use bigint;
     use integer;
 
     sub decode { shift =~ m/$b58\Z/p ? $b58{${^MATCH}} + 58*decode(${^PREMATCH}) : 0 }
-    sub encode { my $_ = shift; return encode($_/58) . (Bitcoin::BASE58)[$_%58] if $_ > 0 } 
+    sub encode { my $_ = shift; return $_ < 58 ? $b58[$_] : encode($_/58) . $b58[$_%58] } 
 }
 
 # Virtual base class for base58-encoded data (with version number and checksum)
@@ -30,10 +35,14 @@ use Digest::SHA qw(sha256);
 
 # public methods
 sub size();		# virtual constant function  (should be automatically inlined)
-sub default_version();	# virtual constant function  (should be automatically inlined)
-sub version;
+sub version();	        # virtual constant function  (should be automatically inlined)
+
+# data and value represent the same data.  They define themselves reciprocally
+# in a circular manner.  One of them MUST be defined in child class to break
+# the circularity.
 sub data;
 sub value;
+
 sub checksum;
 sub to_base58;
 sub to_hex;
@@ -46,58 +55,51 @@ sub to_hex;
 sub _no_class    { my $_ = shift; die "class method call not implemented" unless ref; return $_ }
 sub _no_instance { my $_ = shift; die "instance method call not implemented" if ref;  return $_ }
 
-# stringification overloading
-use overload fallback => 'TRUE', q("") => sub { shift->to_base58 };
-
-# definitions for non virtual methods
-sub value   { shift->_no_class->[0] }
-sub version { my $_ = shift; $_->[1] // $_->default_version }
-sub data    { pack 'H*', shift->_no_class->value->as_hex =~ s/0x//r }
+# stringification
+use overload q("") => sub { shift->to_base58 };
 
 {
     use integer;
     use bigint;
 
-    sub new {
-	my $class = shift->_no_instance;
-	my $arg = shift;
-	my $version = shift;
-	if (ref($arg) eq 'Math::BigInt') { bless [ $arg % 2**$class->size, $version ], $class }
-	elsif (ref($arg) eq $class) { return new $class $arg->value, $version // $arg->version }
-	elsif ($arg =~ s,(?:0x)?([0-9a-f]{@{[$class->size/4]}}),$1,) { new $class hex($arg), $version }
-	elsif ($arg =~ m/\A[@{[Bitcoin::BASE58]}]+\Z/i)	{
-	    my $new = bless [
-		map { $_ / 256**4 % 2**$class->size, $_ / 256**4 / 2**$class->size }
-		Bitcoin::Base58::decode $arg
-	    ], $class;
-	    die 'wrong checksum' if $new->checksum != Bitcoin::Base58::decode($arg) % 256**4;
-	    return $new;
-	}
-	#else  { new $class Bitcoin::Util::btoi($arg), $version }
-	else  {
-	    warn 'argument must be truncated' if length(unpack 'b*', $arg) > $class->size;
-	    new $class hex(unpack 'H'. $class->size * 2, $arg), $version;
-	}
+    sub value {
+	my $this = shift->_no_class;
+	return hex unpack "H*", $this->data;
+    }
+
+    sub data  {
+	my $this = shift->_no_class;
+	return pack 'H*', (2**$this->size+$this->value)->as_hex =~ s/0x1//r;
+    }
+
+    sub value_from_address {
+	my $this = shift->_no_instance;
+	my $decode = Bitcoin::Base58::decode shift;
+	return $decode / 256**4 % 2**$this->size;
     }
 
     sub checksum {
-	my $_ = shift;
-	return ref() ? ref->checksum( $_->version()*2**$_->size + $_->value ) :
-	hex unpack 'H8', Bitcoin::hash pack 'H*',
-	((0x100*2**$_->size + shift)->as_hex =~ s/0x1//r);
+	use Bitcoin::Digest;
+	my $this = shift->_no_class;
+	return hex unpack 'H8', Bitcoin::Digest::hash256_bin pack 'H*',
+	sprintf "%02X%s",
+	$this->version,
+	unpack("H*", $this->data);
     }
 
     sub to_hex {
-	my $_ = shift->_no_class;
-	return +(((0x100 + $_->version) * 2**$_->size + $_->value) * 256**4 + $_->checksum)->as_hex =~ s/0x1//r;
+	my $this = shift->_no_class;
+	return sprintf "%02X%s%08X",
+	$this->version,
+	unpack("H*", $this->data),
+	$this->checksum;
     }
 
     sub to_base58 {
-	my $_ = shift->_no_class;
-	my $total_value = $_->version * 2**$_->size + $_->value;
-	$total_value *= 256 ** 4;
-	$total_value += $_->checksum;
-	return +($_->version > 0 ? '' : '1') . Bitcoin::Base58::encode $total_value;
+	my $this = shift->_no_class;
+	return +($this->version > 0 ? '' : '1') .
+	Bitcoin::Base58::encode
+	hex $this->to_hex;
     }
 
 }
@@ -123,7 +125,7 @@ Bitcoin::Base58
     @ISA = qw(Bitcoin::Base58::Data);
 
     sub size() { 1024 }
-    sub default_version() { 1 }
+    sub version() { 1 }
 
 =head1 DESCRIPTION
 
@@ -133,7 +135,7 @@ It also contains a virtual class, Bitcoin::Base58::Data, which can be used
 to implement the version+checksum system used with bitcoin addresses and private keys.
 
 To do so, you need to inherit your class from Bitcoin::Base58::Data, and then
-define C<size()> and C<default_version()>.  Make sure you explicitely use an
+define C<size()> and C<version()>.  Make sure you explicitely use an
 empty prototype.
 
 =head1 AUTHOR
