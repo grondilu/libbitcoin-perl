e55f9497b591c1292ebdca5d353392cc337bce09
Lucien Grondin <grondilu@redkey>
2012-09-01 05:13:23 +0200

    adding Bitcoin::Constants

---
+++ Bitcoin/Constants.pm
@@ -0,0 +1,6 @@
+package Bitcoin::Constants;
+
+use constant DATA_DIR => '/media/disk27/lucien/bitcoin-test/';
+use constant GENESIS => '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f';
+
+1;
